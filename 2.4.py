0f7a4cd725cdba6eb730f661491bb8bafd7f34fa
Goop0 <affe.gardebro@elev.ga.ntig.se>
2020-09-07 14:56:35 +0200

    Hej

---
+++ 2.4.py
@@ -0,0 +1,7 @@
+svar = input('skriv ett tal: ')
+x = float(svar)
+y = x * x
+l = x+x+x+x 
+print('sida', x)
+print('area', y)
+print('omkrets', l)
---
+++ 2.5.py
@@ -0,0 +1,5 @@
+svar = input('skriv ett tal: ')
+x = float(svar)
+y = x * 125%
+print('med moms', y)
+print('utan moms', x)
