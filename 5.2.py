7034a559b8223cbe0b558a407a660f403a19c916
Goop0 <affe.gardebro@elev.ga.ntig.se>
2020-11-30 14:42:34 +0100

    Update 5.2.py

--- 5.2.py
+++ 5.2.py
@@ -1,4 +1,10 @@
-t = str(input('skriv en text'))
+t = str(input('skriv en text '))
 print(len(t))
-print(t[0])
-print(t[])
+print(f'det första tecknet är {t[0]}')
+h = len(t) 
+h = h -1
+print(f'det sista tecknet är {t[h]}')
+if t[0]==t[h]:
+    print('båda har samma tecken')
+elif t[0] != t[h]:
+    print('båda har inte samma tecken')
